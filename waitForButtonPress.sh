ba08069b27f6196071cc9fdf5319bf7ee91e1ef6
Talha <muhammad.t.k@hotmail.co.uk>
2018-11-23 16:27:38 -0500

    Update waitForButtonPress.sh

--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -4,18 +4,19 @@ gpio mode 5 in
 gpio mode 5 up
 
 a=$(gpio read 5)
-echo $a
 
 while true
 do
     until [ "$a" -eq 0 ]
     do
+        a=$(gpio read 5)
         echo "waiting not pressed"
         sleep 5
     done
     
     until [ "$a" -eq 1 ]
     do
+        a=$(gpio read 5)
         echo "waiting pressed"
         sleep 5
     done
