b37c21dfa3db027a2b107680cad35752d3897762
Talha <muhammad.t.k@hotmail.co.uk>
2018-11-26 11:17:58 -0500

    Update countButtonPress.sh

--- countButtonPress.sh
+++ countButtonPress.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 counter=0
-read=$(gpio read 5)
 bcount=0
 while true
 do
-    read=$(gpio read 5)
-    if [ "$read" -eq 0 ]
-    then
-        #echo $counter
-        ((counter++))
-        ./waitForButtonPress.sh
-        ((bcount++))
-        ./setbits.sh "$counter"
-        echo $bcount
-        if [ "$counter" -eq 16 ]
-        then
-            counter=0
-        fi
+    #echo $counter
+    ((counter++))
+    ./waitForButtonPress.sh
+    ((bcount++))
+    ./setbits.sh "$counter"
+    echo $bcount
 
-        if [ "$counter" -eq 0 ]
-        then
-            gpio write 4 1 # buzzer beeps
-            sleep 1
-            gpio write 4 0
-        fi
+    if [ "$counter" -eq 16 ]
+    then
+        echo "hello world"
+        counter=0
     fi
+
+    if [ "$counter" -eq 0 ]
+    then
+        gpio write 4 1 # buzzer beeps
+        sleep 1
+        gpio write 4 0
+    fi   
 done
